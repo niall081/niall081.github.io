fe4e6a5a3a3722777c38bf19bf6a35667a7954ad
Gloria Mancini <glorialmphoto@gmail.com>
2022-07-29 17:17:52 +0200

    add the home page

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const primaryNav = document.querySelector('.primary-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const navLinks = document.querySelectorAll('.primary-nav__link');
+
+
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    
+    if(visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true)
+        
+    } else if(visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+        
+    }
+})
+
+navLinks.forEach(element => {
+    element.addEventListener('click', ()=> {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false) 
+    })
+})
